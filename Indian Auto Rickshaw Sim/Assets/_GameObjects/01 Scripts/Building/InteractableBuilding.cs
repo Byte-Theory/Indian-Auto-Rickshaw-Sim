b8831c4c137082acb7a282cbccaad4c36bf1619b
Arjunxxxl <a.asankhala@gmail.com>
2025-10-31 19:57:14 +0530

    Petrol pump building added and InteractableBuilding script added

---
+++ Indian Auto Rickshaw Sim/Assets/_GameObjects/01 Scripts/Building/InteractableBuilding.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class InteractableBuilding : MonoBehaviour
+{
+    [SerializeField] private InteractableBuildingType interactableBuildingType;
+}
---
+++ Indian Auto Rickshaw Sim/Assets/_GameObjects/01 Scripts/Building/InteractableBuildingType.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public enum InteractableBuildingType
+{
+    Unknown,
+    PetrolPump
+}
